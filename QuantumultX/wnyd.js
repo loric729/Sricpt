e7f1a297f9b482ad8a2aa99ab7527056eaea4b82
loric729 <52496003+loric729@users.noreply.github.com>
2020-02-25 15:08:29 +0800

    Update wnyd.js

--- QuantumultX/wnyd.js
+++ QuantumultX/wnyd.js
@@ -10,7 +10,7 @@ Surge & QX MITM = p.du.163.com
 var body = $response.body;
 var obj = JSON.parse(body);
 
-obj.tradeEndTime = 1679685290000;
+obj.tradeEndTime = 2524633661;
 body = JSON.stringify(obj);
 $done({body});
 
